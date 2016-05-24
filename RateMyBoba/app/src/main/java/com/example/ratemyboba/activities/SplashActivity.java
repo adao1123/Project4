746dc60517be39b3e9d677aa628a71350c212667
adao1123 <adao1193@gmail.com>
2016-05-23 18:53:13 -0700

    Splash Screen done

--- RateMyBoba/app/src/main/java/com/example/ratemyboba/activities/SplashActivity.java
+++ RateMyBoba/app/src/main/java/com/example/ratemyboba/activities/SplashActivity.java
@@ -2,29 +2,33 @@ package com.example.ratemyboba.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.ratemyboba.R;
 
 public class SplashActivity extends Activity {
     private static boolean splashLoaded = false;
+    private TextView textView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         if (!splashLoaded) {
             setContentView(R.layout.activity_splash);
             int secondsDelayed = 1;
+            setFont();
+
             new Handler().postDelayed(new Runnable() {
                 public void run() {
                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
                     finish();
                 }
-            }, secondsDelayed * 500);
+            }, secondsDelayed * 1000);
 
             splashLoaded = true;
         }
@@ -34,5 +38,10 @@ public class SplashActivity extends Activity {
             startActivity(goToMainActivity);
             finish();
         }
+    }
+    private void setFont(){
+        textView = (TextView)findViewById(R.id.appName_TV_ID);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Amatic_Bold.ttf");
+        textView.setTypeface(typeface);
     }
 }
